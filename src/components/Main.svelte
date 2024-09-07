54d122d0ca0a420ead9d68d23e90e4bfc4ec26a3
Anzar <peerzada.anzar@gmail.com>
2024-09-07 23:12:52 +0530

    added image in main section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,29 @@
-Anzar
+<script>
+
+</script>
+
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 oy-6 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I am <span class="poppins text-violet-400">Anzar</span> Peerzada <br/> Full Stack 
+                <span class="poppins text-violet-400">Developer</span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                I am a Full Stack Developer with a passion for creating beautiful and functional websites. I have experience in building websites using HTML, CSS, JavaScript, and React. I am also proficient in using Node.js, Express, and MongoDB to build the backend of websites. I am always looking for new opportunities to learn and grow as a developer.
+            </p>
+            <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl 
+                poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer" 
+                href="/contact" target="_blank">
+                <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover: translate-x-full z-0 duration-200" />
+                <h4 class="relative z-9">Get in touch &rarr;</h4>
+            </a>
+        </div>
+        
+        <div class="relative shadow-2xl grid place-items-center">
+            <img src={"images/anzar.png"} alt="Anzar Peerzada" class="z-[2] max-h-[70vh] object-cover" />
+        
+    </section>
+
+</main>
