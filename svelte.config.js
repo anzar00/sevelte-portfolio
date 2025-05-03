4191124e4d5afaaf6b80309af7fbb4f0c466c03a
Anzar <peerzada.anzar@gmail.com>
2025-05-04 01:22:26 +0530

    update sevelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import vercel from '@sveltejs/adapter-vercel';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: vercel({
-      runtime: 'nodejs18.x' // Specify Node.js 18 runtime
-    })
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
-
 export default config;
